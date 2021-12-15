8f2a62ab88cb713a669e9c586faa10fd6b7b8781
Dio <dionavialdy@gmail.com>
2021-12-15 17:19:05 +0700

    Update tubes.h

--- tubes.h
+++ tubes.h
@@ -4,5 +4,49 @@
 
 using namespace std;
 
+#define info(P) (P)->info
+#define next(P) (P)->next
+#define prev(P) (P)->prev
+#define kota(P) (P)->kota
+#define first(L) ((L).first)
+#define last(L) ((L).last)
+#define nil NULL
+
+typedef struct elm_parent *adr_parent;
+typedef struct elm_child *adr_child;
+
+struct provinsi{
+    string nama_provinsi;
+    string ibu_kota;
+};
+
+struct datakota{
+    string nama_kota;
+    float suhu;
+};
+
+struct elm_parent{
+    provinsi info;
+    adr_parent next;
+    adr_parent prev;
+    adr_child kota;
+};
+
+struct elm_child{
+    datakota info;
+    adr_child next;
+    adr_child prev;
+};
+
+struct parentDLL{
+    adr_parent first;
+    adr_parent last;
+};
+
+struct childDLL{
+    adr_child first;
+    adr_child last;
+}
+
 
 #endif // TUBES_H_INCLUDED
